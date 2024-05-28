91873bb2fb4e46aee9eb5b4d20c365ef3c0de4f1
koromoko10 <koromoko10@gmail.com>
2024-05-29 00:58:45 +0900

    上に戻るボタン復活&新デザインお試し
    ・上に戻るボタンのバグ修正をして復活させました
    ・新しいデザインのお試しページを作りました（マテリアルデザイン）

---
+++ new/script.js
@@ -0,0 +1,80 @@
+// https://webparts.cman.jp/button/gotop/　を参考に作成しています
+//なぜか動かないので放置しとく
+
+var vGoTop = {};
+function goTop(){
+ 
+  vGoTop["coef"] = 50;  // ←滑らか係数（大きいほど滑らか）
+  vGoTop["cnt"]  = 0;
+ 
+  // --- 現在のスクロール位置取得 -----
+  var startX = document.body.scrollLeft || document.documentElement.scrollLeft;
+  var startY = document.body.scrollTop  || document.documentElement.scrollTop;
+ 
+  // --- スクロールの単位計算 ---------
+  var moveSplitCnt = 0;
+  for(var i = 1; i <= vGoTop["coef"]; i++) {
+    moveSplitCnt += i * i;
+  }
+  vGoTop["unitH"] = startY / ( moveSplitCnt * 2 );
+ 
+  vGoTop["nextX"] = startX;
+  vGoTop["nextY"] = startY;
+ 
+  // --- スクロール開始 ---------------
+  goTopLoop();
+}
+function goTopLoop(){
+ // ============================================================================
+ //  スクロール実行
+ // ============================================================================
+ 
+  vGoTop["cnt"]++;
+ 
+  // --- 次のスクロール位置計算 -------
+  var Coef = 0;
+  if(vGoTop["cnt"] <= vGoTop["coef"]){
+    Coef = vGoTop["cnt"];
+  }else{
+    Coef = ((vGoTop["coef"] * 2) + 1) - vGoTop["cnt"];
+  }
+  vGoTop["nextY"] = vGoTop["nextY"] - Math.round( vGoTop["unitH"] * ( Coef * Coef) );
+  if((vGoTop["cnt"] >= (vGoTop["coef"] * 2))||(vGoTop["nextY"] <= 0)){
+    vGoTop["nextY"] = 0;
+  }
+ 
+  // --- スクロール実行 ---------------
+  window.scrollTo(vGoTop["nextX"], vGoTop["nextY"]);
+ 
+  // --- 次のスクロールを設定 ---------
+  if(vGoTop["nextY"] <= 0){
+    clearTimeout(vGoTop["timer"]);                   // 終了：タイマクリア
+  }else{
+    vGoTop["timer"] = setTimeout("goTopLoop()",10);  // 次のループ
+  }
+}
+window.addEventListener("scroll", goTopDisp, false);
+function goTopDisp(){
+ // ============================================================================
+ //  先頭表示時のボタン消去
+ // ============================================================================
+  var btn  = document.getElementById("topScroll");
+ 
+  var nowY = document.body.scrollTop  || document.documentElement.scrollTop;
+  if(nowY ==0){
+    btn.style.display = "none";
+  }else{
+    btn.style.display = "";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
