20184245b708bb41adcc52fc98a3ef09bb5ebd53
koromoko10 <136338615+koromoko10@users.noreply.github.com>
2025-04-14 00:52:49 +0900

    オフライン表示なぜか動かないので修正
    とりあえずダメ元でscript.jsにもつけた

--- script.js
+++ script.js
@@ -67,3 +67,16 @@ function goTopDisp(){
     btn.style.display = "";
   }
 }
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('✅ Service Worker registered:', reg);
+      })
+      .catch((err) => {
+        console.log('❌ Service Worker registration failed:', err);
+      });
+  });
+}
