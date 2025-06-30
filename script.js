bedcda896699c483328bc4f1c0dddc11f1ab683b
koromoko10 <136338615+koromoko10@users.noreply.github.com>
2025-06-30 14:29:42 +0900

    オフライン表示の削除
    PWAオフライン機能にバグが多かったため削除

--- script.js
+++ script.js
@@ -67,16 +67,3 @@ function goTopDisp(){
     btn.style.display = "";
   }
 }
-
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => {
-        console.log('✅ Service Worker registered:', reg);
-      })
-      .catch((err) => {
-        console.log('❌ Service Worker registration failed:', err);
-      });
-  });
-}
